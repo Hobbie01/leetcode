5701f2c5c4c3d96ee5ef9ddea7e1c8ceaf5ce884
Hobbie01 <61002652+Hobbie01@users.noreply.github.com>
2023-11-02 15:50:12 +0700

    deleted comment

--- PalindromeNumber/index.js
+++ PalindromeNumber/index.js
@@ -1,23 +1,21 @@
-// var isPalindrome = function(x) {
-//     const startTime = new Date();
+var isPalindrome = function (x) {
+  const startTime = new Date();
 
+  const strX = x.toString();
 
-
-//     const endTime = new Date();
-//     const executionTime = endTime - startTime;
-//     console.log(`โปรแกรมที่รันเสร็จในเวลา: ${executionTime} มิลลิวินาที`)
-// };
-
-var isPalindrome = function(x) {
-const strX = x.toString();
-
-for(let i = 0; i < strX.length/2+1; i++) {
-    console.log(`${strX[i]} === ${strX[strX.length-i-1]}`)
-    if (strX[i] !== strX[strX.length-i-1]) return false 
-}
-return true
-
-
+  for (let i = 0; i < strX.length / 2 + 1; i++) {
+    console.log(`${strX[i]} === ${strX[strX.length - i - 1]}`);
+    if (strX[i] !== strX[strX.length - i - 1]) {
+      const endTime = new Date();
+      const executionTime = endTime - startTime;
+      console.log(`โปรแกรมที่รันเสร็จในเวลา: ${executionTime} มิลลิวินาที`);
+      return false;
+    }
+  }
+  const endTime = new Date();
+  const executionTime = endTime - startTime;
+  console.log(`โปรแกรมที่รันเสร็จในเวลา: ${executionTime} มิลลิวินาที`);
+  return true;
 };
 
-module.exports  = isPalindrome
+module.exports = isPalindrome;
